b8d3f1e4d58da50868818494ee15bd5ddf50a633
Abhi Jain <devabhixda@gmail.com>
2020-04-28 13:19:23 +0530

    Add work section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './Home';
 import About from './About';
+import Work from './Work';
 
 class App extends React.Component {
     render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
                 <div>
                     <Route path ="/" exact component={Home}/>
                     <Route path ="/about" exact component={About}/>
+                    <Route path ="/work" exact component={Work}/>
                 </div>
             </Router>
         );
---
+++ src/Work.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import logo from './images/logo.svg';
+import rects from './images/rects.svg';
+import bullets from './images/bullets.svg';
+
+import './stylesheets/work.css';
+
+class Work extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <div className="page">
+                    <div className="aheader">
+                        <a href="https://devabhi.tech">
+                            <img src={logo} className="logo" />
+                        </a>
+                    </div>
+                    <img src={rects} className="rects" />
+                    <div className="work">
+                        Work
+                    </div>                    
+                    <div className="bts">
+                        <img src={bullets} className="bt" />
+                        <span className="title">Founder</span>
+                        <div className="pow">
+                            OzoneApps
+                        </div>
+                        <div className="time">
+                            July 2018-Present
+                        </div>
+                    </div>
+                    <div className="bts">
+                        <img src={bullets} className="bt" />
+                        <span className="title">App Development Intern</span>
+                        <div className="pow">
+                            Hanhas technology and services
+                        </div>
+                        <div className="time">
+                            July 2019-September 2019
+                        </div>
+                    </div>
+                    <div className="bts">
+                        <img src={bullets} className="bt" />
+                        <span className="title">Web Development Intern</span>
+                        <div className="pow">
+                            TechCooper solutions
+                        </div>
+                        <div className="time">
+                            May 2019-June 2019
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Work;
