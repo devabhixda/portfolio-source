33612161304cc6d681cfc8ec668a97e5a36a78f1
Abhi Jain <devabhixda@gmail.com>
2020-04-29 07:55:32 +0530

    Make work page responsive!
    Signed-off-by: Abhi Jain <devabhixda@gmail.com>

--- src/Work.js
+++ src/Work.js
@@ -3,13 +3,79 @@ import logo from './images/logo.svg';
 import rects from './images/rects.svg';
 import bullets from './images/bullets.svg';
 
+import { Icon } from '@iconify/react';
+import menuIcon from '@iconify/icons-dashicons/menu';
+
 import './stylesheets/work.css';
 
 class Work extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isPhone: false,
+        };
+        this.updatePredicate = this.updatePredicate.bind(this);
+    }
+    componentDidMount() {
+        this.updatePredicate();
+        window.addEventListener("resize", this.updatePredicate);
+    }
+
+    updatePredicate() {
+        this.setState({ isPhone: window.innerWidth < 800 });
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updatePredicate);
     }
     render() {
+        const isPhone = this.state.isPhone;
+        if (isPhone) {
+            return (
+                <div className="mpage">
+                    <a href="https://devabhi.tech">
+                        <img src={logo} className="mlogo" />
+                    </a>
+                    <div className="hamburger">
+                        <Icon icon={menuIcon} style={{ color: '#ffffff', fontSize: '40px' }} />
+                    </div>
+                    <img src={rects} className="rects" />
+                    <div className="work">
+                        Work
+                    </div>
+                    <div className="bts">
+                        <img src={bullets} className="bt" />
+                        <span className="title">Founder</span>
+                        <div className="pow">
+                            OzoneApps
+                        </div>
+                        <div className="time">
+                            July 2018-Present
+                        </div>
+                    </div>
+                    <div className="bts">
+                        <img src={bullets} className="bt" />
+                        <span className="title">App Development Intern</span>
+                        <div className="pow">
+                            Hanhas technology and services
+                        </div>
+                        <div className="time">
+                            July 2019-September 2019
+                        </div>
+                    </div>
+                    <div className="bts">
+                        <img src={bullets} className="bt" />
+                        <span className="title">Web Development Intern</span>
+                        <div className="pow">
+                            TechCooper solutions
+                        </div>
+                        <div className="time">
+                            May 2019-June 2019
+                        </div>
+                    </div>
+                </div>
+            )
+        }
         return (
             <div>
                 <div className="page">
@@ -21,7 +87,7 @@ class Work extends React.Component {
                     <img src={rects} className="rects" />
                     <div className="work">
                         Work
-                    </div>                    
+                    </div>
                     <div className="bts">
                         <img src={bullets} className="bt" />
                         <span className="title">Founder</span>
