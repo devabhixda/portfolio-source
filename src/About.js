47d614cd5aea8663f200eb1e43e45af19d515eac
Abhi Jain <devabhixda@gmail.com>
2020-04-29 08:12:29 +0530

    Make about section responsive!

--- src/About.js
+++ src/About.js
@@ -1,17 +1,86 @@
 import React from 'react';
 import logo from './images/logo.svg';
 import about from './images/about.png';
+import mabout from './images/mabout.png';
 import rect from './images/rect.svg';
 import bullets from './images/bullets.svg';
-import Social from './Social';
-
+import { Icon } from '@iconify/react';
+import menuIcon from '@iconify/icons-dashicons/menu';
 import './stylesheets/about.css';
 
 class About extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isPhone: false,
+        };
+        this.updatePredicate = this.updatePredicate.bind(this);
+    }
+    componentDidMount() {
+        this.updatePredicate();
+        window.addEventListener("resize", this.updatePredicate);
+    }
+
+    updatePredicate() {
+        this.setState({ isPhone: window.innerWidth < 800 });
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updatePredicate);
     }
     render() {
+        const isPhone = this.state.isPhone;
+        if (isPhone) {
+            return (
+                <div className="root">
+                    <div className="atop">
+                        <a href="https://devabhi.tech">
+                            <img src={logo} className="mlogo" />
+                        </a>
+                        <div className="hamburger">
+                            <Icon icon={menuIcon} style={{ color: '#ffffff', fontSize: '40px' }} />
+                        </div>
+
+                    </div>
+                    <div className="abottom">
+                    <img src={rect} className="rect" />
+                        <div className="edu">
+                            Education
+                    </div>
+                        <div className="bullets">
+                            <img src={bullets} className="bullet" />
+                            <span className="course">B.Tech Computer Science</span>
+                            <div className="insti">
+                                Shri G.S Intitute of Tech and Science, Indore
+                        </div>
+                            <div className="year">
+                                July 2018-Present
+                        </div>
+                        </div>
+                        <div className="bullets">
+                            <img src={bullets} className="bullet" />
+                            <span className="course">High School</span>
+                            <div className="insti">
+                                Lotus International School, Bangred
+                        </div>
+                            <div className="year">
+                                2017
+                        </div>
+                        </div>
+                        <div className="bullets">
+                            <img src={bullets} className="bullet" />
+                            <span className="course">Middle School</span>
+                            <div className="insti">
+                                Advanced Academy, Indore
+                        </div>
+                            <div className="year">
+                                2015
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            )
+        }
         return (
             <div>
                 <div className="aleft">
